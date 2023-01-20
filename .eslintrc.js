805f6444f3218b65f52da3deb166e0d35b283831
kostanios <kotov.const@mail.ru>
2023-01-20 13:09:28 +0100

    edit eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'module',
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+        es2020: true,
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.ts', '.json'],
+            },
+        },
+    },
+};
--- src/commands/alerts.ts
+++ src/commands/alerts.ts
@@ -1,11 +1,10 @@
-import Logger from "bunyan";
+import Logger from 'bunyan';
 
 export default async (args: any[], logger: Logger) => {
-    try {
-
-    }
-catch (err) {
-        logger.error( 'Could not process specific hour', err);
-        process.exit(err.code);
-    }
-}
+  try {
+    /* empty */
+  } catch (err) {
+    logger.error('Could not process specific hour', err);
+    process.exit(err.code);
+  }
+};
